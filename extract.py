8d6ad070c0e89e4b208ff143f65141ea702fa313
maroon-spec <gont.jun@gmail.com>
2021-08-31 12:36:58 +0900

    Add files via upload

---
+++ extract.py
@@ -0,0 +1,34 @@
+from PIL import Image
+import os
+import sys
+import re
+
+def extract(src):
+	img = Image.open(src)
+	rgb_img = img.convert("RGB")
+	img_size = rgb_img.size
+
+	arr = []
+	index = 0
+	binary = ''
+	for x in range(img_size[0]):
+		for y in range(img_size[1]):
+			r, g, b = img.getpixel((x, y))
+			rgb = [r, g, b]
+			for c in rgb:
+				if index == 7:
+					codepoint = int(binary, 2)
+					t = chr(codepoint)
+					arr.append(t)
+					binary = ''
+					index = 0
+				binary += bin(c)[len(bin(c))-1]
+				index = index + 1
+	ex = "".join(arr)
+	print(ex[0:31])
+
+if __name__ == '__main__':
+	args = sys.argv
+	dir = os.path.dirname(__file__)
+	img_path = os.path.join(dir, args[1])
+	extract(img_path)
